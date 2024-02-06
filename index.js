f9ad971bf3a55ab9c944be8f87d10cff850e06d5
virajhmehta <virajmehta2499@gmail.com>
2024-02-06 17:14:03 +0530

    git hub data Added

--- index.js
+++ index.js
@@ -1,25 +1,65 @@
-require('dotenv').config()
-const express = require('express')
+require("dotenv").config();
+const express = require("express");
 // import express from "express" line 1 == 2
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const githubData = {
+  login: "virajhmehta",
+  id: 40292937,
+  node_id: "MDQ6VXNlcjQwMjkyOTM3",
+  avatar_url: "https://avatars.githubusercontent.com/u/40292937?v=4",
+  gravatar_id: "",
+  url: "https://api.github.com/users/virajhmehta",
+  html_url: "https://github.com/virajhmehta",
+  followers_url: "https://api.github.com/users/virajhmehta/followers",
+  following_url:
+    "https://api.github.com/users/virajhmehta/following{/other_user}",
+  gists_url: "https://api.github.com/users/virajhmehta/gists{/gist_id}",
+  starred_url:
+    "https://api.github.com/users/virajhmehta/starred{/owner}{/repo}",
+  subscriptions_url: "https://api.github.com/users/virajhmehta/subscriptions",
+  organizations_url: "https://api.github.com/users/virajhmehta/orgs",
+  repos_url: "https://api.github.com/users/virajhmehta/repos",
+  events_url: "https://api.github.com/users/virajhmehta/events{/privacy}",
+  received_events_url:
+    "https://api.github.com/users/virajhmehta/received_events",
+  type: "User",
+  site_admin: false,
+  name: null,
+  company: null,
+  blog: "",
+  location: null,
+  email: null,
+  hireable: null,
+  bio: null,
+  twitter_username: null,
+  public_repos: 12,
+  public_gists: 0,
+  followers: 4,
+  following: 2,
+  created_at: "2018-06-15T09:19:26Z",
+  updated_at: "2024-02-06T11:35:03Z",
+};
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-app.get('/', (req, res) => {
-  res.send('iviraj')
-})
+app.get("/github", (req, res) => {
+  res.json(githubData);
+});
+app.get("/", (req, res) => {
+  res.send("iviraj");
+});
 
-app.get('/login',(req, res)=>{
-    res.send("<h1>Hello Login</h1>")
-})
+app.get("/login", (req, res) => {
+  res.send("<h1>Hello Login</h1>");
+});
 
-app.get('/youtube',(req, res)=>{
-    res.send("JS Backend")
-})
+app.get("/youtube", (req, res) => {
+  res.send("JS Backend");
+});
 
 app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
